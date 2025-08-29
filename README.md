025278ad5b97a8611f70810e31efec39946a5da5
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-08-29 11:26:10 +0100

    setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fx_brokers_trading
+
+fx_brokers_trading is a collection of forex brokers or trading website 
+
---
+++ server/connection.php
@@ -0,0 +1,60 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function checkUrlProtocol($url)
+{
+    $parsedUrl = parse_url($url);
+    return isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] : 'invalid';
+}
+
+// Automatically get the current URL
+$currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")
+    . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+// Get the protocol from the current URL
+$request = checkUrlProtocol($currentUrl);
+
+// Default configurations
+define("HOST", "localhost");
+
+// Determine if online or offline
+$isLocalhost = ($_SERVER['HTTP_HOST'] === 'localhost');
+
+// Database connection (Only use one based on environment)
+// $connection = '';
+
+if ($isLocalhost) {
+    // Offline (Localhost)
+    $domain = "http://localhost/consignment_websites/";
+
+    define("USER", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "forex");
+
+    // Database connection
+    $connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+} else {
+    // Online (Live Server)
+    $domain = "http://upsnlt.com/";
+
+    define("USER", "");
+    define("PASSWORD", "");
+    define("DATABASE", "");
+
+    // Database connection
+    $connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+}
+
+
+$siteshort = 'Forex Hub';
+$sitename = 'Forex Hub International Limited';
+$siteemail = 'support@fhil.com';
+
+$money = '&#36;';
