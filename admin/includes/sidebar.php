fee06fa997413ccff9bba3c8cf73970784ab3bd3
arum kingsley <arumkingsley06@gmail.com>
2025-09-03 11:16:53 -0700

    working

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,38 @@
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title">Menu</li>
+
+                            <li>
+                                <a href="" class="waves-effect">
+                                    <i class="ri-dashboard-line"></i>
+                                    <span>Dashboard</span>
+                                </a>
+                            </li>
+                            
+                            <li>
+                                <a href="" class="waves-effect">
+                                    <i class="ri-account-circle-line"></i>
+                                    <span>Users</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="" class="waves-effect">
+                                    <i class="ri-wallet-3-line"></i>
+                                    <span>Deposits</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="" class="waves-effect">
+                                    <i class="ri-wallet-3-line"></i>
+                                    <span>Withdrawals</span>
+                                </a>
+                            </li>
+
+
+
+
+                        </ul>
+                    </div>
