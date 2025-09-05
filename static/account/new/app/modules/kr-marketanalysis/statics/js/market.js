000a8b23e8a1f8292c8fd2daa90a1f17f6cf02e8
arum kingsley <arumkingsley06@gmail.com>
2025-09-04 20:14:21 -0700

    design problem fixed

---
+++ static/account/new/app/modules/kr-marketanalysis/statics/js/market.js
@@ -0,0 +1,11 @@
+function initMarketList() {
+    initMarketNav();
+
+    $('.kr-marketlist-item').off('click').click(function() {
+        changeView('coin', 'coin', {
+            symbol: $(this).attr('kr-symbol-mm'),
+            currency: $(this).attr('kr-symbol-tt'),
+            market: $(this).attr('kr-symbol-market')
+        }, null, true);
+    });
+}
