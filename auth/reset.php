75d29cc91ba1c9ad72901b32fedeb4e2e0be4ccd
arum kingsley <arumkingsley06@gmail.com>
2025-09-22 18:18:05 -0700

    working

--- auth/reset.php
+++ auth/reset.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
@@ -7,7 +7,8 @@
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="viewport" content="width=device-width, user-scalable=no">
 
-<meta property="og:description" content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
+<meta property="og:description"
+    content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
 <meta property="”og:image”" content="”/static/globalcityindex-live/wp-content/themes/vt/favicon-new.png”">
 <meta property="”og:title”" content="cityindex-live">
 
@@ -19,43 +20,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- System properties -->
-  
+
     <meta name="page-name" content="login">
     <meta name="user-id" content="">
     <meta http-equiv="refresh" content="7200">
     <title>Forget Password | Cityindex-live</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="canonical" href="../login.html">
-<link rel="alternate" hreflang="en" href="../login.html">
+    <link rel="canonical" href="../login.html">
+    <link rel="alternate" hreflang="en" href="../login.html">
 
-<meta name="description" content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
-<meta property="og:title" content="Log In | cityindex-live">
-<meta property="og:url" content="/login">
-<meta property="og:type" content="website">
-<meta property="og:site_name" content="cityindex-live">
+    <meta name="description"
+        content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
+    <meta property="og:title" content="Log In | cityindex-live">
+    <meta property="og:url" content="/login">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="cityindex-live">
 
-<meta property="og:description" content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
-<meta name="twitter:card" content="summary">
-<meta name="twitter:site" content="@cityindex-live">
-<meta name="twitter:title" content="Log In | cityindex-live">
+    <meta property="og:description"
+        content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@cityindex-live">
+    <meta name="twitter:title" content="Log In | cityindex-live">
 
-<meta name="twitter:description" content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
+    <meta name="twitter:description"
+        content="cityindex-live is a Financial Organization set up to Provide Source of Passive income for investors on Real Estate Management and Natural Oil & Gas">
     <!-- State -->
     <script type="text/javascript">
-        
-        
-            </script>
 
-           
-            <link rel="icon" type="image/png" sizes="16x16" href="./static/globalcityindex-live/wp-content/themes/vt/favicon-new.png">
-            <link rel="manifest" href="./static/globalcityindex-live/Content/ico/manifest.json">
-            <meta name="facebook-domain-verification" content="yr09tc1ug4r9h5lcpyh59lg5kawf4k" />
-            <meta name="msapplication-TileColor" content="#ffffff">
-            <meta name="msapplication-TileImage" content="/static/globalcityindex-live/wp-content/themes/vt/favicon-new.png">
 
-            <!-- Fonts -->
+    </script>
+
+
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="./static/globalcityindex-live/wp-content/themes/vt/favicon-new.png">
+    <link rel="manifest" href="./static/globalcityindex-live/Content/ico/manifest.json">
+    <meta name="facebook-domain-verification" content="yr09tc1ug4r9h5lcpyh59lg5kawf4k" />
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/static/globalcityindex-live/wp-content/themes/vt/favicon-new.png">
+
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com/">
     <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&amp;display=swap" rel="stylesheet">
     <link rel="icon" href="./static/globalcityindex-live/wp-content/themes/vt/favicon-new.png">
@@ -64,63 +69,65 @@
     <!-- Styles -->
     <link rel="stylesheet" href="../static/globalcityindex-live/assets/assets/css/app2d1f.css">
     <link rel="stylesheet" href="../static/globalcityindex-live/assets/assets/sass/app2a6f.css">
-    </head>
+</head>
+
 <body>
-    
+
     <div id="app" data-page-name="login" v-bind:class="{ 'overflow-hidden h-screen': ! $store.state.scroll }">
-            <div class="layout-minimal">
-        <a href="../index.html">
+        <div class="layout-minimal">
+            <a href="">
 
-        </a>
+            </a>
 
-        <div class="layout-minimal-inner">
-            <div class="title-wrapper">
-                 	<!--messages-->
-<div class="messages" style="text-align: center;">
-	
-	</div>
-  <!--end messages-->
-                <h2>
+            <div class="layout-minimal-inner">
+                <div class="title-wrapper">
+                    <!--messages-->
+                    <div class="messages" style="text-align: center;">
+
+                    </div>
+                    <!--end messages-->
+                    <h2>
                         I forgot my password
-                </h2>
+                    </h2>
+                </div>
+                <form method="POST" action="" class="login-form">
+                    <input type="hidden" name="csrfmiddlewaretoken"
+                        value="SW6NL4Mmib3J1aGsB1jsenGyLybzGDbiQs30fLLLkXvucmISeSxYo3Yeyle7LkTV">
+                    <div class="form-group">
+                        <v-label label-for="email">E-mail address</v-label>
+
+                        <input type="email" name="email" autocomplete="email" maxlength="254" required
+                            placeholder="Please enter your email address" value="" name="email" class="custom-input"
+                            id="email">
+                        <span style="color: red; list-style:none;  "></span>
+                    </div>
+
+                    <div class="submit-btn-wrapper">
+
+
+
+
+                        <button type="submit" class="btn btn-primary">Reset</button>
+                    </div>
+                </form>
             </div>
-            <form method="POST"
-            action="index.html"
-            class="login-form"
-            >
-            <input type="hidden" name="csrfmiddlewaretoken" value="SW6NL4Mmib3J1aGsB1jsenGyLybzGDbiQs30fLLLkXvucmISeSxYo3Yeyle7LkTV">
-            <div class="form-group">
-                <v-label label-for="email">E-mail address</v-label>
-                
-     <input type="email" name="email" autocomplete="email" maxlength="254" required placeholder="Please enter your email address" value="" name="email" class="custom-input" id="email">
-     <span style="color: red; list-style:none;  "></span>
-            </div>
-
-        <div class="submit-btn-wrapper">
-            
-
-
-
-            <button type="submit" class="btn btn-primary">Reset</button>
         </div>
-    </form>
-        </div>
+
+
+
+
     </div>
-
-
-        
-                   
-                        </div>
 
     <!-- Scripts -->
     <div id="scripts">
         <script src="./static/globalcityindex-live/assets/assets/js/manifestf3c5.js" defer></script>
-<script src="./static/globalcityindex-live/assets/assets/js/vendor313f.js" defer></script>
-<script src="./static/globalcityindex-live/assets/assets/js/appc73e.js" defer></script>
+        <script src="./static/globalcityindex-live/assets/assets/js/vendor313f.js" defer></script>
+        <script src="./static/globalcityindex-live/assets/assets/js/appc73e.js" defer></script>
     </div>
 
 
-    
-    
+
+
 </body>
+
 </html>
